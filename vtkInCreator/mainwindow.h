34632a5b364eb3fd055e5e233343615e6146b5a6
0422 <32591123+0422@users.noreply.github.com>
2025-09-17 13:56:13 +0800

    Add files via upload

---
+++ vtkInCreator/mainwindow.h
@@ -0,0 +1,40 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include "global_headers.h"
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    // Renderer
+    vtkSmartPointer<vtkRenderer> main_render_3d;
+
+    // Render Window
+    vtkSmartPointer<vtkGenericOpenGLRenderWindow> renWin3D;
+    // vtkSmartPointer<vtkGenericOpenGLRenderWindow> getRenderWindow();
+
+    vtkSmartPointer<vtkActor> current_actor;
+    vtkSmartPointer<vtkUnstructuredGrid> ugrid;
+
+
+private slots:
+    void on_pushButton_loadfile_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
